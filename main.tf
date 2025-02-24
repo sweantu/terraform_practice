0223483cd744177231b07283d68b4dd3647af87f
Anh Tu <sweantu@gmail.com>
2025-02-24 07:56:00 +0700

    feat: terraform bucket

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.21.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "de-zoomcamp-practice-451708"
+  region  = "asia-southeast1-b"
+}
+
+resource "google_storage_bucket" "terraform-bucket" {
+  name          = "de-zoomcamp-practice-451708-terraform-bucket"
+  location      = "asia"
+  force_destroy = true
+}
